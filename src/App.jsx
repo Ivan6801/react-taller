b510c89d9bfa58a2a91973eb7444dd1d93db8c57
Ivan Gonzalez <hal308366@gmail.com>
2023-05-06 12:02:23 -0700

    add: clase 1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline bg-red-400">Hello world!</h1>
+    </div>
+  );
+}
