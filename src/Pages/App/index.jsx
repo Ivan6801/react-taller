09cd375833f3810976f82c50ac7254cada2c732c
Ivan Gonzalez <hal308366@gmail.com>
2023-05-09 07:16:29 -0700

    create to routers

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,19 +1,47 @@
 import React from "react";
+import { BrowserRouter, useRoutes } from "react-router-dom";
 import Home from "../Home";
 import MyAccount from "../MyAccount";
 import MyOrder from "../MyOrder";
 import MyOrders from "../MyOrders";
 import SignIn from "../SignIn";
+import NotFound from "../NotFound";
 import "./App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/my-account",
+      element: <MyAccount />,
+    },
+    {
+      path: "/my-order",
+      element: <MyOrder />,
+    },
+    {
+      path: "/my-orders",
+      element: <MyOrders />,
+    },
+    {
+      path: "/sign-in",
+      element: <SignIn />,
+    },
+    {
+      path: "/*",
+      element: <NotFound />,
+    },
+  ]);
+  return routes;
+};
 
 export default function App() {
   return (
-    <div className="bg-red-400 text-black">
-      <Home />
-      <MyAccount />
-      <MyOrder />
-      <MyOrders />
-      <SignIn />
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
