fae70222cc74d8035a7948e3d27784ad0abf5393
Ivan Gonzalez <igonzalez@grupoexcel.com>
2023-05-16 18:28:53 -0700

    add to card component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import Card from "../../components/Card";
 
 export default function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      Home
+      <Card />
+    </Layout>
+  );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,10 @@
-/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
+
 const Layout = ({ children }) => {
+  Layout.propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
   return <div className="flex flex-col items-center mt-20">{children}</div>;
 };
 
