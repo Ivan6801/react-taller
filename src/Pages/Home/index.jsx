86a5e36e8bc646bf0da38cd7b782b13f49fe7f20
Ivan Gonzalez <igonzalez@grupoexcel.com>
2023-05-16 19:02:18 -0700

    add to api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import Card from "../../components/Card";
 
 export default function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </div>
     </Layout>
   );
 }
