88a638c6b14f1cbbb07eb10017504b08e53da757
Ivan Gonzalez <hal308366@gmail.com>
2023-06-29 15:19:08 -0700

    FIX: change position

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <Layout>
       Home
-      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg pt-2">
         {items?.map((item) => (
           <Card key={item.id} data={item} />
         ))}
