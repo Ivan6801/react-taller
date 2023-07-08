9fe6d8ccb8d6276e522e9a022c05f32a697511ba
Ivan Gonzalez <igonzalez@grupoexcel.com>
2023-07-07 21:45:41 -0700

    feat(frontend): Evitando productos duplicados en el carrito

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { PlusIcon, CheckIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context/index";
 
 const Card = (data) => {
@@ -26,13 +26,34 @@ const Card = (data) => {
     setCartProducts([...cartProducts, productData]);
     openCheckoutSideMenu();
     closeProductDetail();
-    console.log("CART: ", cartProducts);
+  };
+
+  const renderIcon = (id) => {
+    const isInCart =
+      cartProducts.filter((product) => product.id === id).length > 0;
+
+    if (isInCart) {
+      return (
+        <div className="absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1">
+          <CheckIcon className="h-6 w-6 text-white"></CheckIcon>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+          onClick={(event) => addProductsToCart(event, data.data)}
+        >
+          <PlusIcon className="h-6 w-6 text-black"></PlusIcon>
+        </div>
+      );
+    }
   };
 
   return (
     <div
       className="bg-white cursor-pointer w-56 h-60 rounded-lg"
-      onClick={(event) => addProductsToCart(event, data.data)}
+      onClick={() => showProduct(data.data)}
     >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
@@ -43,12 +64,7 @@ const Card = (data) => {
           src={data.data.images[0]}
           alt={data.data.title}
         />
-        <div
-          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-          onClick={(event) => addProductsToCart(event, data.data)}
-        >
-          <PlusIcon className="h-6 w-6 text-black"></PlusIcon>
-        </div>
+        {renderIcon(data.data.id)}
       </figure>
       <p className="flex justify-between">
         <span className="text-sm font-light">{data.data.title}</span>
--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -1,11 +1,13 @@
 import { useContext } from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import { ShoppingCartContext } from "../../Context";
+import OrderCard from "../OrderCard";
 import "./styles.css";
 
 const CheckoutSideMenu = () => {
-  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu } =
+  const { isCheckoutSideMenuOpen, closeCheckoutSideMenu, cartProducts } =
     useContext(ShoppingCartContext);
+  console.log("CART: ", cartProducts);
 
   return (
     <aside
@@ -21,6 +23,16 @@ const CheckoutSideMenu = () => {
             onClick={() => closeCheckoutSideMenu()}
           ></XMarkIcon>
         </div>
+      </div>
+      <div className="px-6 overflow-y-scroll">
+        {cartProducts?.map((product) => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+          />
+        ))}
       </div>
     </aside>
   );
